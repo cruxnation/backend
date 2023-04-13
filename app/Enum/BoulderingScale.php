692f0d45dcf0ab843de177dfeeee69bffce49a47
Václav Pelíšek <peldax@gmail.com>
2023-04-13 08:55:11 +0000

    Create BoulderingScale.php

---
+++ app/Enum/BoulderingScale.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Enum;
+
+use \Graphpinator\Typesystem\Attribute\Description;
+
+enum BoulderingScale : string
+{
+    #[Description('Fontainebleau grading scale (6C+, 7A, 7A+, ...)')]
+    case FONT = 'FONT';
+    #[Description('Hueco grading scale (V4, V5, V6, ...)')]
+    case HUECO = 'HUECO';
+}
