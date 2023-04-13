777a5930c0fcd451983355a650f01afe858f6ec1
Václav Pelíšek <peldax@gmail.com>
2023-04-13 11:14:39 +0200

    Rockscale enum

---
+++ app/Enum/RockScale.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Enum;
+
+use \Graphpinator\Typesystem\Attribute\Description;
+
+enum RockScale : string
+{
+    #[Description('French grading scale (6c+, 7a, 7a+, ...)')]
+    case FRENCH = 'FRENCH';
+    #[Description('UIAA grading scale (8, 8+, 9-, ...)')]
+    case UIAA = 'UIAA';
+}
